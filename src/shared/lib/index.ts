cc406e6a959769a0419bf5e387b095164cf66c86
maga <maga777rama@gmail.com>
2024-11-28 02:55:50 +0400

    add find-pizza

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -5,3 +5,4 @@ export { getCartDetails } from "./get-cart-details";
 export { calcCartItemTotalPrice } from "./calc-cart-item-total-price";
 export { updateCartTotalAmount } from "./update-cart-total-amount";
 export { findOrCreateCart } from "./find-or-create-cart";
+export { findPizzas } from "./find-pizzas";
